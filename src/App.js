8bd5d57048c252de135efb1a3153b0bb5649f779
ali masoomzadeh <eng.masoomzadeh@gmail.com>
2020-11-15 13:04:05 +0330

    create app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+import CounterApp from './CounterApp'
+
+class App extends Component{
+  render() {
+    return(
+        <CounterApp/>
+    );
+  }
+}
+
+export default App;
