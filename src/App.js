4227875a0f56c79a3923ae612f14c17ed7bebf77
ali masoomzadeh <eng.masoomzadeh@gmail.com>
2020-11-15 13:31:58 +0330

    add Provider and Store in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React, {Component} from 'react'
-import CounterApp from './CounterApp'
+import React, {Component} from "react"
+import CounterApp from "./CounterApp"
+import {Provider} from "react-redux"
+import Store from "./redux/Store"
 
-class App extends Component{
-  render() {
-    return(
-        <CounterApp/>
-    );
-  }
+class App extends Component {
+    render() {
+        return (
+            <Provider store={Store}>
+                <CounterApp/>
+            </Provider>
+        );
+    }
 }
 
 export default App;
